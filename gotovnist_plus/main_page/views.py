aaf505ab586c66942ae1051b535137a890d344f2
Mykhailo Romanenko <mixa251106@gmail.com>
2025-10-24 20:35:35 +0300

    simple main and about page

--- gotovnist_plus/main_page/views.py
+++ gotovnist_plus/main_page/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h4> Testing main page</h4>')
+
+def about(request):
+    return HttpResponse('<h4> About main page</h4>')
